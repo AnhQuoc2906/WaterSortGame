33dedba9312e978f1076e313beaa9d225dd33ada
Manh3012 <vuvanmanh3012@gmail.com>
2023-03-10 21:15:15 +0700

    Add congratulations

--- Assets/Script/GameController.cs
+++ Assets/Script/GameController.cs
@@ -1,11 +1,18 @@
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using Unity.Burst.CompilerServices;
 
 public class GameController : MonoBehaviour
 {
     public BottleController FirstBottle;
     public BottleController SecondBottle;
+    public GameObject canvas;
+    public GameObject canvas2;
+    public GameObject canvas3;
+    public GameObject canvas4;
+    public GameObject canvas5;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -54,9 +61,33 @@ public class GameController : MonoBehaviour
                                 FirstBottle = null;
                                 SecondBottle = null;
                             }
+                            Congratulation(FirstBottle);
+
                         }
+
                     }
+
                 }
+            }
+        }
+       
+
+    }
+    public void Congratulation(BottleController FirstBottle)
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+        if (hit.collider != null)
+        {
+            FirstBottle = hit.collider.GetComponent<BottleController>();
+            if (FirstBottle.numberOfColorsInBottle == 2)
+            {
+                canvas.SetActive(true);
+                canvas2.SetActive(false);
+                canvas3.SetActive(false);
+                canvas4.SetActive(false);
+                canvas5.SetActive(false);
             }
         }
     }
